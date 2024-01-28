f65513266ecf53752e9b5ffd2260eba01b12b1ab
Louis Duret-Robert <louisduret@gmail.com>
2024-01-28 16:32:43 +0100

    Fix some bugs + injection status FSM that can be read from APB + other changes

--- make/j2gpp/filters/filters.py
+++ make/j2gpp/filters/filters.py
@@ -1,2 +1,12 @@
 def hexadecimal(value):
   return f"{value:X}"
+
+def arrsize(size):
+  """
+  Generates the `[size-1:0]` signal array size definition if `size>1`,
+  else it returns an empty string.
+  """
+  if size == 1:
+    return ""
+  else:
+    return f"[{size-1}:0]"
