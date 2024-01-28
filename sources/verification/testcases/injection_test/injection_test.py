5140965bff63646ca5394fcf2fbef99942037ea9
Louis Duret-Robert <louisduret@gmail.com>
2024-01-28 16:33:23 +0100

    Basic Injection testcase

---
+++ sources/verification/testcases/injection_test/injection_test.py
@@ -0,0 +1,49 @@
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.handle import SimHandleBase
+from cocotb.triggers import ClockCycles
+
+import cocotbext.apb as apb
+
+@cocotb.test()
+async def injection_test(dut:SimHandleBase):
+  master = apb.APBMasterDriver(dut, "control", dut.control_pclk, uppercase=False)
+
+  cocotb.start_soon(Clock(dut.control_pclk, 10, units="ns").start())
+  cocotb.start_soon(Clock(dut.data_aclk, 1, units="ns").start())
+
+  dut.control_presetn.value = 0
+  dut.data_aresetn.value = 0
+  await ClockCycles(dut.control_pclk, 2)
+  dut.control_presetn.value = 1
+  dut.data_aresetn.value = 1
+  await ClockCycles(dut.control_pclk, 2)
+
+  await ClockCycles(dut.control_pclk, 20)
+
+  # Configure
+  await master.busy_send(apb.APBTransaction(address =  0xC, data =    0x1)) # Injection mode
+  await master.busy_send(apb.APBTransaction(address = 0x10, data = 0x1000)) # Start address
+  await master.busy_send(apb.APBTransaction(address = 0x18, data = 0x2000)) # Stop address
+  await master.busy_send(apb.APBTransaction(address = 0x20, data =   0x20)) # Address increment
+
+  await ClockCycles(dut.control_pclk, 5)
+
+  await master.busy_send(apb.APBTransaction(address =  0x4, data =    0x1)) # Start
+
+  await ClockCycles(dut.control_pclk, 5)
+
+  pooling_iterations = 100
+  while pooling_iterations != 0:
+    transaction = apb.APBTransaction(address = 0x8) # Status
+    await master.busy_send(transaction)
+    if ((transaction.data & 0b11) == 0b11):
+      break
+    pooling_iterations -= 1
+
+  await ClockCycles(dut.control_pclk, 10)
+
+  await master.busy_send(apb.APBTransaction(address =  0x4, data =    0x0)) # Reset
+
+  await ClockCycles(dut.control_pclk, 100)
