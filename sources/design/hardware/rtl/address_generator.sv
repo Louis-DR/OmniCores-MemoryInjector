af3a8f0c901ef84543ff43d476a3e0f7f8bb6344
Louis Duret-Robert <louisduret@gmail.com>
2024-02-05 23:18:29 +0100

    Fix data and address generator enable

--- sources/design/hardware/rtl/address_generator.sv
+++ sources/design/hardware/rtl/address_generator.sv
@@ -24,11 +24,13 @@ logic lfsr_feedback;
 
 always_comb begin
   address_next = address;
-  if (mode_selector) begin
-    lfsr_feedback = ^(address & lfsr_fibonacci_taps);
-    address_next = lfsr_mask & { lfsr_feedback , ( {WIDTH-1{lfsr_feedback}} & lfsr_gallois_taps ) ^ address[WIDTH-1:1] };
-  end else begin
-    address_next = address_next + range_increment;
+  if (enable) begin
+    if (mode_selector) begin
+      lfsr_feedback = ^(address & lfsr_fibonacci_taps);
+      address_next = lfsr_mask & { lfsr_feedback , ( {WIDTH-1{lfsr_feedback}} & lfsr_gallois_taps ) ^ address[WIDTH-1:1] };
+    end else begin
+      address_next = address_next + range_increment;
+    end
   end
 end
 
