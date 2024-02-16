cf205c745ab997a84ec22dedc9613cfd9a55af04
Louis Duret-Robert <louisduret@gmail.com>
2024-02-16 23:31:17 +0100

    Add padding for header generation of register bank

--- make/j2gpp/adapters/register_bank_adapter.py
+++ make/j2gpp/adapters/register_bank_adapter.py
@@ -1,16 +1,22 @@
 
 def register_bank_adapter(vars):
   register_offset = 0
+  previous_offset = 0
   for register in vars['registers']:
     if 'offset' in register:
       register_offset = register['offset']
     elif 'align' in register:
       register_offset = (register_offset // register['align'] + 1) * register['align']
       register['offset'] = register_offset
-      register_offset += 4
     else:
       register['offset'] = register_offset
-      register_offset += 4
+    register_offset += 4
+
+    if register['offset'] != 0:
+      padding_before = register['offset'] - previous_offset - 4
+      if padding_before:
+        register['padding_before'] = padding_before
+      previous_offset = register['offset']
 
     if 'fields' in register:
       # Add the offset for register fields
